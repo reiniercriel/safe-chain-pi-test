8c7871a4b138333352c552e0149d411d99ca4fe8
Reinier Criel <reinier.criel@gmail.com>
2025-10-28 07:02:16 -0700

    Rename tests

---
+++ src/safe_chain_pi_test/__init__.py
@@ -0,0 +1,5 @@
+"""safe-chain-pi-test: tiny package for demo"""
+
+from .draw import draw_bug, print_bug
+
+__all__ = ["draw_bug", "print_bug"]
---
+++ src/safe_chain_pi_test/draw.py
@@ -0,0 +1,18 @@
+def draw_bug():
+    """Draw a cute bug on the console.
+    
+    Returns the bug ASCII art as a string.
+    """
+    bug = r"""
+       /\_/\
+      ( o.o )
+       > ^ <
+      /|   |\
+     (_|   |_)
+    """
+    return bug
+
+
+def print_bug():
+    """Print a bug to the console."""
+    print(draw_bug())
